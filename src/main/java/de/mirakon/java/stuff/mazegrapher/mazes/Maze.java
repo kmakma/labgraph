e29f8812ca46603523ecc890e8f00af50a4625d4
Michael <michaelo1@gmx.de>
2017-05-03 19:09:08 +0200

    formatierung/kommentare

--- src/main/java/de/mirakon/java/stuff/mazegrapher/mazes/Maze.java
+++ src/main/java/de/mirakon/java/stuff/mazegrapher/mazes/Maze.java
@@ -1,11 +1,19 @@
 /*
  * Copyright 2017 Michael
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 package de.mirakon.java.stuff.mazegrapher.mazes;
@@ -13,18 +21,16 @@ package de.mirakon.java.stuff.mazegrapher.mazes;
 import org.jetbrains.annotations.NotNull;
 
 public interface Maze {
-
+    // LabyrinthA , ~B, ~D sind die vielversprechendsten
 
     // TODO: 20.03.2017 newInstance mit argumenten hinzufügen
-    @NotNull
-    Maze newInstance();
+    @NotNull Maze newInstance();
 
-    @NotNull
-    String getMazeCategory();
+    @NotNull String getMazeCategory();
 
-    @NotNull
-    String getMazeName();
+    @NotNull String getMazeName();
 
-    @NotNull
-    String getMazePlugin();
+    @NotNull String getMazePlugin();
+
+    // TODO: 02.05.2017 String getDescription();
 }
