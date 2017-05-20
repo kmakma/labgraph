bafa1c1c60a53b743f1a175726507fe241f1745e
Michael <michaelo1@gmx.de>
2017-05-21 00:31:51 +0200

    änderungen an generate, und buildfix (da hier noch überbleibsel der GenericCoordinates waren)

--- src/main/java/de/mirakon/java/mazegrapher/plugins/PrimAlgoMazeL2.java
+++ src/main/java/de/mirakon/java/mazegrapher/plugins/PrimAlgoMazeL2.java
@@ -19,10 +19,16 @@
 package de.mirakon.java.mazegrapher.plugins;
 
 import de.mirakon.java.mazegrapher.main.AbstractMaze;
-import de.mirakon.java.mazegrapher.main.GenericCoordinates;
+import de.mirakon.java.mazegrapher.main.Coordinates;
 import de.mirakon.java.mazegrapher.main.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * A maze of this class is created by using a randomized version of prim's algorithm (sort of creating a minimal
@@ -34,6 +40,8 @@ public class PrimAlgoMazeL2 extends AbstractMaze {
     private static final String mazeCategory = "Perfect Mazes";
     private static final String mazeName = "PrimAlgoL2";
     private static final String mazePlugin = "default";
+    private int height;
+    private int width;
 
     @SuppressWarnings("unused")
     public PrimAlgoMazeL2() {
@@ -69,9 +77,21 @@ public class PrimAlgoMazeL2 extends AbstractMaze {
             throw new IllegalArgumentException(Strings.getString(Strings.ERROR_MAZE_GENERATION_BAD_SIZE, height,
                     width));
         }
-        GenericCoordinates[][] maze = new GenericCoordinates[height][width];
+        this.height = height;
+        this.width = width;
+        Map<Coordinates, Boolean> mazes = new HashMap<>();
+
         // TODO: 20.05.2017 befülle maze mit standardzeug
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // TODO: 21.05.2017 create walls everywhere
+            }
+        }
+
         // TODO: 20.05.2017 erstes feld zufällig wählen
+        int zufX = ThreadLocalRandom.current().nextInt(1, height - 1);
+        int zufY = ThreadLocalRandom.current().nextInt(1, width - 1);
+        Set<Coordinates> coordinatesWithPossibleConnections = new HashSet<>();
         // TODO: 20.05.2017 erste feld zu nodesWithPossibleConnections hinzufügen
         // TODO: 20.05.2017 findPossibleConnections, und aktualisiere dabei nWPC
         // TODO: 20.05.2017 wähle connection füg sie hinzu und wiederhole
